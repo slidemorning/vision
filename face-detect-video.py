7e7b96a56824f3d55e9c14c459080adc5dfc8fd5
slidemorning <givertion2@gmail.com>
2020-11-08 02:01:59 +0900

    Haarcascade face classifier on webcam

--- face-detect-video.py
+++ face-detect-video.py
@@ -1,7 +1,8 @@
 import sys
 import cv2
 
-xmlpath = sys.path[0] + '/haarcascade_frontalface_alt2.xml'
+
+xmlpath = sys.path[0] + '/xml/haarcascade_frontalface_alt2.xml'
 
 cap = cv2.VideoCapture(0)
 
@@ -9,10 +10,12 @@ if not cap.isOpened():
     print('Fail to open video')
     sys.exit()
 
+# Object of CascadeClassifier
+# Load xml
 classifier = cv2.CascadeClassifier(xmlpath)
 
 if classifier.empty():
-    print('Fail to load XML')
+    print('Fail to load xml file')
     sys.exit()
 
 while True:
@@ -32,13 +35,15 @@ while True:
     
     for (x, y, w, h) in faces:
 
+		# Draw rectangle of face and put text
         cv2.rectangle(frame, (x, y, w, h), (255, 0, 0), 2)
         cv2.putText(frame, str(ms)+'ms', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-        msg = '====> detect face{}x{}'.format(w, h)
+        msg = '====> detect face{}x{} time:{}'.format(w, h, ms)
         print(msg)
 
     cv2.imshow('frame', frame)
     
+	# ESC
     if cv2.waitKey(100) == 27:
         break
 
